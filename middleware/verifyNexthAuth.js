8b470c7c19cacc445bc2425d8f268091f9f7c526
Shar-Devvv <200ksuscribers@gmail.com>
2025-10-24 19:53:17 +0530

    Updated middleware

--- middleware/verifyNexthAuth.js
+++ middleware/verifyNexthAuth.js
@@ -6,7 +6,7 @@ export async function verifyNextAuthToken(req, res, next) {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) return res.status(401).json({ error: "Missing token" });
 
-    const secret = new TextEncoder().encode(process.env.NEXTAUTH_SECRET);
+    const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET);
     const { payload } = await jwtVerify(token, secret);
 
     // Optional: get fresh role from DB for higher security
